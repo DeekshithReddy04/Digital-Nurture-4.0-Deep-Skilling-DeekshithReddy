fc0073b55b2c03b14420bf53765941ded58f4375
DeekshithReddy04 <doladeekshithreddy@gmail.com>
2025-07-26 16:05:07 +0530

    Add files via upload

---
+++ week6/7.ReactJS-HOL/shoppingapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import OnlineShopping from "./OnlineShopping";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <OnlineShopping />
+      </div>
+    );
+  }
+}
+
+export default App;
