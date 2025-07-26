a007f26b18754842724cba16c6261a0ce891a935
DeekshithReddy04 <doladeekshithreddy@gmail.com>
2025-07-26 16:00:37 +0530

    Add files via upload

---
+++ week6/6.ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from "./trainer";
+
+const trainers = [
+  new Trainer("t-syed8", "syed khaleelullah", "khaleelullah@cognizant.com", "92819289210", ".net", ["c#", "sql server","React",".NET Core"]),
+  new Trainer("t-jojo", "jojo jose", "jojo@congnizant.com", "98832927765", "Java", ["JAVA", "JSP","ANGULAR","SPRING"]),
+  new Trainer("t-elisa","ELISA JONES","Elisa@cognizant.com","9839692925","Python",["Python","Django","Angular"])
+];
+
+export default trainers;
